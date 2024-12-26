f10571e9b40e8d23820932a595cb8bfb43e90663
HARSH825 <harshchhallani3@gmail.com>
2024-12-27 01:42:46 +0530

    add models

---
+++ backend/models/userSchema.js
@@ -0,0 +1,35 @@
+import mongoose, { mongo } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        required:true,
+        unique:true,
+        trim:true,
+        lowercase:true,
+        minLength:3,
+        maxLength:12
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6
+    },
+    firstName:{
+        type:String,
+        required:true,
+        trim:true,
+        maxLength:30
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    }
+
+});
+
+const User = mongoose.model('User',userSchema);
+
+export {User};

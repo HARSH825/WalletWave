011f4b5210ecd1dacbdeab77cf55cb3dc8279144
HARSH825 <harshchhallani3@gmail.com>
2024-12-27 01:35:45 +0530

    Setup server

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
-const express = require("express");
+import express from 'express';
+import dotenv from 'dotenv';
+import {connectDb} from './config/connectDb.js';
 
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+connectDb();
+const PORT = process.env.PORT || 3001;
+app.listen(PORT,()=>{
+    console.log("Server Running on Port : "+PORT);
+});
 

566d3b9aab4a8a7fffb2823d72d3c64eb2e3155b
HARSH825 <harshchhallani3@gmail.com>
2024-12-27 18:56:40 +0530

    attach userId in request

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import {connectDb} from './config/connectDb.js';
 import  userRouter  from './routes/userRouter.js';
+import  accountRouter from './routes/accountRouter.js';
 import cors from 'cors';
 
 app.use(cors());
@@ -10,6 +11,7 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/v1/',userRouter);
+app.use('/api/v1/account',accountRouter);
 
 connectDb();
 const PORT = process.env.PORT || 3001;

df4c7f3d3a5cb6551b9ca70655085d168b01e7e5
HARSH825 <harshchhallani3@gmail.com>
2024-12-27 17:37:21 +0530

    fix minor issue

--- backend/middlewares/authentication.js
+++ backend/middlewares/authentication.js
@@ -1,12 +1,14 @@
 //login
 
 import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const authMiddleware = (req,res,next)=>{
     const header = req.headers.authorization;
 
     if(!header || !header.startsWith('Bearer')){
-        return res.json({msg:"Invalid credentials"});
+        return res.status(411).json({msg:"Invalid credentials"});
     }
 
     const token = header.split(' ')[1];
@@ -15,16 +17,18 @@ const authMiddleware = (req,res,next)=>{
         const decoded = jwt.verify(token,process.env.JWT_SECRET);
 
         if(decoded.userId){
-            req.userId = decoded.userId;
+            req.userId = decoded.userId; //add userid in the request .
             next();
         }
         else{
-            return res.json(404).json({msg:"Failed to verify User via token"});
+            return res.status(403).json(404).json({msg:"Failed to verify User via token"});
         }
     }
     catch(err){
-        console.log("Failed to decode , invalid credentials : "+err);
+        console.log("Failed to decode  : "+err.message);
+        return res.status(403).json({msg:"Invalid token, access denied"});
     }
 }
 
+export {authMiddleware};
 //sgart here tomorroe

5efb0544e7af492b9248f9d663c979814229db01
HARSH825 <harshchhallani3@gmail.com>
2024-12-27 17:37:57 +0530

    add zod validation

--- backend/validation/zod.js
+++ backend/validation/zod.js
@@ -1,10 +1,19 @@
 import zod from 'zod';
 
 const signupSchema = zod.object({
-    username:zod.string(),
-    password:zod.string(),
-    firstName:zod.string(),
-})
+    username:zod.string().min(1),
+    password:zod.string().min(6),
+    firstName:zod.string().min(1),
+});
 
+const updateSchema = zod.object({
+    firstName:zod.string().optional(),
+    lastName: zod.string().optional(),
+    password:zod.string().optional()
+});
 
-export {signupSchema};
+const signInSchema = zod.object({
+    username:zod.string().min(1),
+    password:zod.string().min(6),
+});
+export {signupSchema,updateSchema, signInSchema};
